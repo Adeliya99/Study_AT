f99a2b663483c1d2e7d1ca5dd32ca9290c56fcac
Adel-sun <adeliy.abushahmanova@gmail.com>
2020-04-12 01:00:51 +0300

    homework4 added

---
+++ Day_4/src/Sample2.java
@@ -0,0 +1,69 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.*;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+
+public class Sample2 extends Assert {
+    WebDriver driver = new ChromeDriver();
+    String baseUrl = "http://demo.guru99.com/test/login.html";
+
+
+    @BeforeTest
+            public void launchBrowser() {
+        driver.get(baseUrl);
+    }
+
+
+    @Test
+            public void sendLogin() {
+        // Get the WebElement corresponding to the Email Address(TextField)
+        Assert.assertTrue(driver.findElement(By.id("email")).isDisplayed());
+        WebElement email = driver.findElement(By.id("email"));
+
+        // Get the WebElement corresponding to the Password Field
+        Assert.assertTrue(driver.findElement(By.name("passwd")).isDisplayed());
+        WebElement password = driver.findElement(By.name("passwd"));
+
+        email.sendKeys("abcd@gmail.com");
+        password.sendKeys("abcdefghlkjl");
+        System.out.println("Text Field Set");
+
+        // Deleting values in the text box
+        email.clear();
+        password.clear();
+        System.out.println("Text Field Cleared");
+
+        // Find the submit button
+        Assert.assertTrue(driver.findElement(By.id("SubmitLogin")).isDisplayed());
+        WebElement login = driver.findElement(By.id("SubmitLogin"));
+
+        // Using click method to submit form
+        email.sendKeys("abcd@gmail.com");
+        password.sendKeys("abcdefghlkjl");
+        login.click();
+        System.out.println("Login Done with Click");
+
+        //using submit method to submit the form. Submit used on password field
+        driver.get(baseUrl);
+        Assert.assertTrue(driver.findElement(By.id("email")).isDisplayed());
+        driver.findElement(By.id("email")).sendKeys("abcd@gmail.com");
+        Assert.assertTrue(driver.findElement(By.name("passwd")).isDisplayed());
+        driver.findElement(By.name("passwd")).sendKeys("abcdefghlkjl");
+        Assert.assertTrue(driver.findElement(By.id("SubmitLogin")).isDisplayed());
+        driver.findElement(By.id("SubmitLogin")).submit();
+        System.out.println("Login Done with Submit");
+    }
+
+
+    @AfterTest
+            public void closeDriver() {
+        driver.close();
+    }
+}
---
+++ Day_4/src/Sample3.java
@@ -0,0 +1,47 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class Sample3 {
+
+        String baseUrl = "http://demo.guru99.com/test/upload/";
+        WebDriver driver = new ChromeDriver();
+        WebDriverWait wait = new WebDriverWait(driver, 20);
+
+        @BeforeTest
+                public void launchBrowser() {
+            driver.get(baseUrl);
+        }
+
+        @Test
+                public void sendFile() {
+            Assert.assertTrue(driver.findElement(By.id("uploadfile_0")).isDisplayed(), "uploadElement is displayed");
+            WebElement uploadElement = driver.findElement(By.id("uploadfile_0"));
+            // enter the file path onto the file-selection input field
+            uploadElement.sendKeys("C:\\Testtext.txt");
+
+
+            // check the "I accept the terms of service" check box
+            driver.findElement(By.id("terms")).click();
+
+            WebElement resultElement = driver.findElement(By.cssSelector("#res"));
+            WebElement resultText = driver.findElement(By.cssSelector("#res > center"));
+
+            // click the "UploadFile" button
+            driver.findElement(By.name("send")).click();
+            wait.until(ExpectedConditions.attributeContains(resultElement, "style", "display: block"));
+            Assert.assertTrue(resultText.getText().contains(" successfully"));
+        }
+
+        @AfterTest
+                public void closeDriver() {
+            driver.quit();
+        }
+}
